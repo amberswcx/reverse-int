cbfc2d7c28d2a3fb6aaeab9791583eb34f75cc70
Pasha <amberswcx@gmail.com>
2021-08-22 17:03:39 +0400

    fix: fix problem with negative numbers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 module.exports = function reverse(n) {
     let reversedN = '';
+
+    if (n < 0) {
+        n = -n;
+    }
+
     while (n >= 1) {
         reversedN += n % 10;
         n = Math.floor(n / 10);
