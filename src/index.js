fda7308c646ecac3464cb9371232eb6130adeeaa
Pasha <amberswcx@gmail.com>
2021-08-22 16:37:35 +0400

    feat: finish 1st solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let reversedN = '';
+    while (n >= 1) {
+        reversedN += n % 10;
+        n = Math.floor(n / 10);
+    }
+    return +reversedN;
 }
